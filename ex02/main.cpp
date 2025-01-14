713710937c698adb605bea3ca8d6a9abf6553c57
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2025-01-14 01:05:09 +0100

    Update main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -13,26 +13,27 @@
 #include <iostream>
 
 int main( void ) {
-//	Fixed a;
-//	Fixed const b( Fixed( 5.05f ) + Fixed( 2 ) );
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-//	std::cout << a << std::endl;
-//	std::cout << ++a << std::endl;
-//	std::cout << a << std::endl;
-//	std::cout << a++ << std::endl;
-//	std::cout << a << std::endl;
-//	std::cout << b << std::endl;
-//	std::cout << Fixed::max( a, b ) << std::endl;
-//
-	
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+
+	/*
 	
 	std::cout << "-------------------------- Aritmetic operations" << std::endl;
 	
-	Fixed a = Fixed( 5 );
+	Fixed a = Fixed( 5.7f );
 	Fixed b = Fixed( 2 );
 	Fixed c;
 
-
+	c = a;
+	c++;
 	std::cout << "a=" << a.toInt() << std::endl;
 	std::cout << "b=" << b << std::endl;
 	std::cout << "c=" << c << std::endl;
@@ -47,14 +48,9 @@ int main( void ) {
 
 	std::cout << "-------------------------- Aritmetic operations WITH OVERFLOW" << std::endl<< std::endl<< std::endl;
 	Fixed zz = Fixed( 8388610 );
-	Fixed aa = Fixed( 8388605 );
+	Fixed aa = Fixed( 8388607 );
 	Fixed bb = Fixed(  200000 );
-	Fixed ee = Fixed( 1.0f );
-	Fixed ff = Fixed( 1.0f + 0.00000005f);
-	Fixed gg = Fixed( 1.0f + 0.00000015f);
-	Fixed hh = Fixed( 1.0f + 0.00000115f);
-	Fixed ii = Fixed( 1.0f + 0.00000915f);
-	Fixed jj = Fixed( 1.0f + 0.00009915f);
+
 	Fixed cc;
 	std::cout << "aa=" << aa.toInt() << std::endl;
 	std::cout << "bb=" << bb << std::endl;
@@ -73,12 +69,30 @@ int main( void ) {
 	std::cout << aa << " <= " << bb << " = " << (aa <= bb) << std::endl;
 	std::cout << aa << " >  " << bb << " = " << (aa >  bb) << std::endl;
 	std::cout << aa << " >= " << bb << " = " << (aa >= bb) << std::endl;
-	std::cout << ee << " >= " << ff << " = " << (ee >= ff) << std::endl;
-	std::cout << ff << " ff==ee " << ee << " = " << (ff == ee) << std::endl;
-	std::cout << gg << " gg==ee " << ee << " = " << (gg == ee) << std::endl;
-	std::cout << hh << " hh==ee " << ee << " = " << (hh == ee) << std::endl;
-	std::cout << ii << " ii==ee " << ee << " = " << (ii != ee) << std::endl;
-	std::cout << jj << " jj==ee " << ee << " = " << (jj != ee) << std::endl;
+
+	Fixed ee = Fixed( 1.00000000000f);
+	Fixed ff = Fixed( 1.001950000000f);
+	Fixed gg = Fixed( 1.001960000000f);
+	Fixed hh = Fixed( 1.001970000000f);
+	Fixed ii = Fixed( 1.001980000000f);
+	Fixed jj = Fixed( 1.002000000000f);
+	std::cout << ee.toFloat() << " >= " << ff.toFloat() << " = " << (ee >= ff) << std::endl;
+	std::cout << ff.toFloat() << " ff==ee " << ee << " = " << (ff == ee) << std::endl;
+	std::cout << gg.toFloat() << " gg==ee " << ee << " = " << (gg == ee) << std::endl;
+	std::cout << hh.toFloat() << " hh==ee " << ee << " = " << (hh == ee) << std::endl;
+	std::cout << ii.toFloat() << " ii==ee " << ee << " = " << (ii == ee) << std::endl;
+	std::cout << jj.toFloat() << " jj==ee " << ee << " = " << (jj == ee) << std::endl;
+	int idx = 0;
+	Fixed initi = Fixed(1.0000000f);
+	Fixed limit = Fixed(1.0002000f);
+	Fixed incre = Fixed(1.0000300f);
+	Fixed refer = Fixed(1.0001000f );
+	for (Fixed i = initi;i <=  limit;i = i +  incre)
+	{
+		std::cout << i << "loop "<< idx++ << " i==ee " << refer << " = " << (i == refer) << std::endl;
+	}
+	
+
 
 
 
@@ -89,5 +103,24 @@ int main( void ) {
 	std::cout << "Max (" << a <<" , 42.5f) = " << Fixed::max(a, 42.5f) << std::endl;
 	std::cout << "Min (" << a <<" , 42) = " << Fixed::min(a, 42) << std::endl;
 	std::cout << "Min (" << a <<" , 42.5f) = " << Fixed::min(a, 42.5f) << std::endl;
+
+	std::cout << "-------------------------- PRE-POST AUTO IN/DE CREMENT" << std::endl<< std::endl<< std::endl<< std::endl;
+	std::cout << "bb=" << bb <<" ,bb++ ==> bb= "<< bb++  << std::endl;
+	std::cout << "bb=" << bb <<" ,bb++ ==> bb= "<< bb++  << std::endl;
+	std::cout << "bb=" << bb <<" ,bb++ ==> bb= "<< bb++  << std::endl;
+	std::cout << "bb=" << bb <<" ,--bb ==> bb= "<< --bb  << std::endl;
+	std::cout << "bb=" << bb <<" ,--bb ==> bb= "<< --bb  << std::endl;
+	std::cout << "bb=" << bb <<" ,--bb ==> bb= "<< --bb  << std::endl;
+		
+	std::cout << "aa=" << aa <<" ,aa++ ==> aa= "<< aa++  << std::endl;
+	std::cout << "aa=" << aa <<" ,aa++ ==> aa= "<< aa++  << std::endl;
+	std::cout << "aa=" << aa <<" ,aa++ ==> aa= "<< aa++  << std::endl;
+		std::cout << "aa=" << aa <<" ,aa++ ==> aa= "<< aa++  << std::endl;
+			std::cout << "aa=" << aa <<" ,aa++ ==> aa= "<< aa++  << std::endl;
+				std::cout << "aa=" << aa <<" ,aa++ ==> aa= "<< aa++  << std::endl;
+	std::cout << "aa=" << aa <<" ,--aa ==> aa= "<< --aa  << std::endl;
+	std::cout << "aa=" << aa <<" ,--aa ==> aa= "<< --aa  << std::endl;
+	std::cout << "aa=" << aa <<" ,--aa ==> aa= "<< --aa  << std::endl;
+	*/
 	return 0;
 }
